7449ba7c55a415efcd45477a431e688048d7aa59
Bruno Ferreira Cangussu <bruno.cangussu@hotmail.com>
2018-06-05 01:45:06 -0300

    Utils relocated

---
+++ src/utils/MaskedInput.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {mask} from '../utils';
+
+export default function MaskedInput (props) {
+    let {mask: pattern, value, onChange, clearRegex} = props;
+    let clearMask = (value) => {
+        return value.replace(clearRegex, '');
+    }
+    let handleMask = (value) => {
+        if (!pattern) return value;
+        let fragmentCapture = /{(\d*)}/g;
+        let partials = pattern.split(fragmentCapture);
+        partials = partials.reduce((previous, current, index) => {
+            if (index % 2) {
+                previous[1].push(current)
+            } else {
+                previous[0].push(current)
+            }
+            return previous;
+        }, [[], []]);
+        partials[1].push(value);
+        return mask(partials[0], ...partials[1]);
+
+    }
+    return (
+        <input
+            type='text' 
+            {...props}
+            onChange={(event) => {
+                onChange(event, clearMask(event.target.value));
+            }}
+            value={handleMask(value)}
+        />
+    )
+}
